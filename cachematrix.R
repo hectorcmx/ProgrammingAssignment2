8ae0e0fa5499a2bb89934e98064cbe5a5c1f6428
hectorcm <hrafaelcalvillo@hotmail.com>
2015-11-22 17:44:27 -0600

    Hw2
    last version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix containes 4 functions set, get, setinverse, getinverse.
+## get.-returns de vector x    set.- change the vectore stored in the main function just in case of a new valor
+## the variable m is used to determinate when the matrix inverse has been already calculated
+## getinverse and setinverse are used to put the value in m variable
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve check if the valu of m is empty in that case it calculate the inverse, otherwaze returns the value that has been already calated
 
 cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
